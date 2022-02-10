3079cf0c60634d65e788974ea7e4d30a2b342853
Hafizzy <hafizzy.ahmad@tm.com.my>
2022-02-10 11:46:10 +0800

    add react route for navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Browser Router untuk wrap kan semua component router yang dalam app.js tu
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
